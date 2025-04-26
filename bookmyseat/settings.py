1af66fd1cbe76c622ad9a4b6c7bfcb7f74b29a57
abhignaprabodh <abhignaprabodh2304@gmail.com>
2025-04-26 10:32:25 +0530

    first commit

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -150,3 +151,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Additional locations of static files
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+from django.core.files.storage import FileSystemStorage
+
+MEDIA_ROOT = '/tmp/media/'  # Temporary writable directory
+
+DEFAULT_FILE_STORAGE = FileSystemStorage(location=MEDIA_ROOT)
